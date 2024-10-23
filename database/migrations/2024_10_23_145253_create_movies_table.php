83b4a1584f9a61e55af09bd5b69c66373d767a29
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-10-23 20:29:19 +0200

    first commit

---
+++ database/migrations/2024_10_23_145253_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->string('thumb',350);
+            $table->string('description',500);
+            $table->date('date');
+            $table->string('director',30);
+            $table->string('country',20);
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
